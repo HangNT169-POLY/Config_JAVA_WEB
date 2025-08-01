52efbf56633453b118688d08aaa052d4e9dc0a0f
HangNT169 <104345338+HangNT169@users.noreply.github.com>
2025-08-01 10:52:41 +0700

    Create JwtUtil.java

---
+++ JwtUtil.java
@@ -0,0 +1,53 @@
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey secretKey =
+            Keys.hmacShaKeyFor("summer_25-block_2-sof3062-sd20201".getBytes());
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hrs
+                .signWith(secretKey, Jwts.SIG.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey).build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token, String username) {
+        String extractedUsername = extractUsername(token);
+        return extractedUsername.equals(username) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expiration = Jwts.parser()
+                .verifyWith(secretKey).build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+        return expiration.before(new Date());
+    }
+
+    public static void main(String[] args) {
+        JwtUtil jwtUtil = new JwtUtil();
+        String token = jwtUtil.generateToken("user1");
+        System.out.println("JWT: " + token);
+        String username = jwtUtil.extractUsername(token);
+        System.out.println("Username from JWT: " + username);
+    }
+}
